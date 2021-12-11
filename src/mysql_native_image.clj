e0d1bd1ca8305d7aee2d1c30ef25c1f9f549333a
John Shaffer <jshaffer2112@protonmail.com>
2021-12-11 12:47:03 -0600

    Add test for with-transaction
    Trying to debug
    https://github.com/babashka/babashka-sql-pods/issues/47. This also
    serves as a good test that basic functionality works.

--- src/mysql_native_image.clj
+++ src/mysql_native_image.clj
@@ -8,7 +8,24 @@
    :password "local"
    :user "root"})
 
+(defn test-with-transaction!
+  "Test for https://github.com/babashka/babashka-sql-pods/issues/47
+
+  No issues here, but it fails in babashka."
+  [datasource]
+  (let [con (jdbc/get-connection datasource)]
+    (prn (jdbc/execute! con ["drop table if exists foo"]))
+    (prn (jdbc/execute! con ["create table foo (a int)"]))
+    (prn (jdbc/execute! con ["insert into foo values (1), (2), (3)"]))
+    (prn (jdbc/execute! con ["select * from foo"]))
+    (jdbc/with-transaction [tx datasource]
+      (Thread/sleep 200)
+      (prn (jdbc/execute! tx ["select * from foo"]))
+      (assert (= (jdbc/execute! con ["select * from foo"])
+                (jdbc/execute! tx ["select * from foo"]))))))
+
 (defn -main [& _]
   (let [ds (jdbc/get-datasource db)]
     (prn
-      (jdbc/execute! ds ["select now()"]))))
+      (jdbc/execute! ds ["select now()"]))
+    (test-with-transaction! ds)))
